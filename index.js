61f843e4a41f987e9f5c647967c38b27b8f33b4a
Jan Str <janstr06@gmail.com>
2023-04-02 18:44:26 +0200

    base

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const csv = require('csvtojson');
 
 const fs = require("fs"); 
 const path = require("path");
+const res = require("express/lib/response");
 
 const port = 8080;
+
+const filePath = path.join(__dirname, 'data/pocasi.csv');
 
 const validWeather = new Set(['jasno', 'skoro jasno', 'polojasno', 'skoro zataženo', 'zataženo']);
 
@@ -20,26 +23,29 @@ app.set("views", path.join(__dirname, "views"));
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 app.post('/data', urlencodedParser, (req, res) => {
-    let info = `"${req.body.pocasi}","${req.body.datum}","${req.body.teplota}`;
+    let info = `"${req.body.datum}","${req.body.pocasi}","${req.body.teplota}"\n`;
     if (!validWeather.has(req.body.pocasi)) {
         return res.status(400).json({
             success: false,
             message: "Neplatné počasí"
         });
     }
+    
     if(!moment(req.body.datum, "YYYY-MM-DD", true).isValid()) {
         return res.status(400).json({
             success: false,
             message: "Neplatné datum"
         });
     }
+    
     if(isNaN(req.body.teplota)) {
         return res.status(400).json({
             success: false,
             message: "Neplatná teplota"
         });
     }
-    fs.appendFile(path.join(__dirname, 'data/pocasi.csv'), str, function(err) {
+
+    fs.appendFile(filePath, info, function(err) {
         if (err) {
           console.error(err);
           return res.status(500).json({
@@ -49,6 +55,15 @@ app.post('/data', urlencodedParser, (req, res) => {
         }
     });
     res.redirect(302, '/');
+});
+
+app.get('/data', (req, res) => {
+    csv({headers: ['datum', 'pocasi', 'teplota']})
+    .fromFile(filePath)
+    .then(data => {
+        console.log(data);
+        res.render('data', {pocasi: data});
+    });
 });
 
 app.listen(port, () => {
